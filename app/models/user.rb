9a12b94314ca6b8de61d9396a94e4f136d67f7dd
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-05 16:04:02 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,5 +9,5 @@ class User < ApplicationRecord
   validates :first_name_kanji,  presence: true
   validates :family_name_kana,  presence: true
   validates :first_name_kana,   presence: true
-  validates :birthday           presence: true
+  validates :birthday,          presence: true
 end
