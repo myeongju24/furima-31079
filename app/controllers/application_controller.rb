9ff03d5c318828f15c35f782b1494b181e16cf84
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-06 12:38:32 +0900

    basic認証の導入

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,20 @@
 class ApplicationController < ActionController::Base
+<<<<<<< Updated upstream
+=======
+  before_action :basic_auth
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :family_name_kanji, :first_name_kanji, :family_name_kana, :first_name_kana, :birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
+    end
+  end
+  
+>>>>>>> Stashed changes
 end
