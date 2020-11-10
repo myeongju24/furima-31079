a19eec53f78a448814dcbf801f43ca0f2a870a3d
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-10 19:16:32 +0900

    with_optionへの変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,13 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  validates :nickname,          presence: true
-  validates :family_name_kanji, presence: true
-  validates :first_name_kanji,  presence: true
-  validates :family_name_kana,  presence: true
-  validates :first_name_kana,   presence: true
-  validates :birthday,          presence: true
+
+  with_options presence: true do
+    validates :nickname
+    validates :family_name_kanji
+    validates :first_name_kanji
+    validates :family_name_kana
+    validates :first_name_kana
+    validates :birthday
+  end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
