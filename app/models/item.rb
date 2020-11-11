ca7ce018e26ae25442c1f04068ebe883c9aacee5
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-11 19:44:31 +0900

    modelファイルへの記載
    model:userファイルへのアソシエーションの記載
    
    model:itemファイルへのバリデーション及びアソシエーションの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,14 @@
 class Item < ApplicationRecord
+  with_options presence: true do
+    validates :title
+    validates :explanation
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :location_id
+    validates :price
+    validates :day_to_delivery_id
+  end
+
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,8 @@ class User < ApplicationRecord
     validates :birthday
   end
 
+  has_many :items
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
 end
