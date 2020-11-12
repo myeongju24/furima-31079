3fa44371058d15562a890ff3b9a08c0dd8397233
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-12 18:29:26 +0900

    発送元の地域のアクティブハッシュ
    商品投稿画面の発送元の地域へのアクティブハッシュの適用

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,8 +2,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :explanation
-    validates :delivery_fee_id
-    validates :location_id
     validates :price
     validates :day_to_delivery_id
   end
@@ -15,8 +13,10 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_fee
+  belongs_to :location
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :delivery_fee_id, numericality: { other_than: 1 }
+  validates :location, numericality: { other_than: 1 }
 end
