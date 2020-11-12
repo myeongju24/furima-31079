478728401a01c0597b8bae8c2ba57a7d5d6ee5ec
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-12 18:41:23 +0900

    発送までの日数のアクティブハッシュ
    商品投稿画面の発送までの日数のアクティブハッシュの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,7 +3,6 @@ class Item < ApplicationRecord
     validates :title
     validates :explanation
     validates :price
-    validates :day_to_delivery_id
   end
 
   belongs_to :user
@@ -14,9 +13,11 @@ class Item < ApplicationRecord
   belongs_to :condition
   belongs_to :delivery_fee
   belongs_to :location
+  belongs_to :day_to_delivery
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
   validates :delivery_fee_id, numericality: { other_than: 1 }
-  validates :location, numericality: { other_than: 1 }
+  validates :location_id, numericality: { other_than: 1 }
+  validates :day_to_delivery_id, numericality: { other_than: 1 }
 end
