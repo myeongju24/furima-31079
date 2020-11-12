6d8763e11cac0db7388730a426940d29d01aaa7c
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-12 17:15:13 +0900

    カテゴリーのアクティブハッシュ
    商品投稿画面のカテゴリーに対するアクティブハッシュの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,7 +2,6 @@ class Item < ApplicationRecord
   with_options presence: true do
     validates :title
     validates :explanation
-    validates :category_id
     validates :condition_id
     validates :delivery_fee_id
     validates :location_id
@@ -12,5 +11,10 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :category_id, numericality: { other_than: 1 }
 
 end
