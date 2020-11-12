bd2695becd1352003782e3b37e0011384927df52
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-12 19:16:45 +0900

    createアクションの更新とストロングパラメーターの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,12 +10,13 @@ class ItemsController < ApplicationController
   end
 
   def create
+    Item.create(item_params)
   end
 
   private
 
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :explanation, :category_id, :condition_id, :delivery_fee_id, :location_id, :price, :day_to_delivery_id,).merge(user_id: current_user.id)
   end
 
 end
