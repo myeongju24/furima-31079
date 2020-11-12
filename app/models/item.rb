d11b8e133974ad8779ee00ccc7fd869ab72a75b2
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-12 18:10:28 +0900

    配送料の負担のアクティブハッシュ
    商品投稿画面の配送料の負担のアクティブハッシュの導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,8 +14,9 @@ class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :condition
+  belongs_to :delivery_fee
 
   validates :category_id, numericality: { other_than: 1 }
   validates :condition_id, numericality: { other_than: 1 }
-
+  validates :delivery_fee_id, numericality: { other_than: 1 }
 end
