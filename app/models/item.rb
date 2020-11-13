6e72c7dfc56df1e13b13381ff528699f651072ec
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-13 19:08:29 +0900

    値段のvalidation
    値段が300~9999999円になるためのバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,5 +21,6 @@ class Item < ApplicationRecord
   validates :location_id, numericality: { other_than: 1 }
   validates :day_to_delivery_id, numericality: { other_than: 1 }
 
-  validates :price, presence: true 
+  validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
+
 end
