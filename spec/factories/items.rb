98fcc5f56cc6fc3cde24329cda70e97da82207af
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-13 21:02:50 +0900

    正常系テストコード

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :item do
-    
+    title              {"テスト"}
+    explanation        {"テスト"}
+    category_id        {2}
+    condition_id       {2}
+    delivery_fee_id    {2}
+    location_id        {2}
+    day_to_delivery_id {2}
+    price              {1000}
+    association :user 
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
-RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Item do
+  
+  before do
+    @item = FactoryBot.build(:item)
+    @item.image = fixture_file_upload('app/assets/images/furima-intro01.png')
+  end
+
+  describe '商品投稿' do
+    context '商品投稿に成功するとき' do
+      it '全ての必須項目を入力すれば投稿できる' do
+        expect(@item).to be_valid
+      end
+    end
+  end
+
 end
