b54b29c5b02f30d0a0a7e7f524ed56e721401e0c
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-14 12:21:51 +0900

    バリデーションの修正
    with_optionsを使ってのバリデーションファイルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,11 +15,13 @@ class Item < ApplicationRecord
   belongs_to :location
   belongs_to :day_to_delivery
 
-  validates :category_id, numericality: { other_than: 1 }
-  validates :condition_id, numericality: { other_than: 1 }
-  validates :delivery_fee_id, numericality: { other_than: 1 }
-  validates :location_id, numericality: { other_than: 1 }
-  validates :day_to_delivery_id, numericality: { other_than: 1 }
+  with_options numericality: { other_than: 1 } do
+    validates :category_id
+    validates :condition_id
+    validates :delivery_fee_id
+    validates :location_id
+    validates :day_to_delivery_id
+  end
 
   validates :price, numericality: { only_integer: true, greater_than: 299, less_than: 10000000 }
 
