96a847a57b33a829a81364f558fd6e48392f435b
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-17 20:24:12 +0900

    都道府県のactivehash
    購入ページの住所の都道府県をアクティブハッシュにするためのコード

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,10 +1,15 @@
 class Address < ApplicationRecord
+
+  validates :postal_code, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 }
   with_options presence: true do
-    validates :postal_code
-    validates :prefecture_id
     validates :city
     validates :street
     validates :phone_number
   end
+
   belongs_to :order
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :address
+
 end
