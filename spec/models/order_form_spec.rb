62c4f27e6e18732e22676a880cff23b61f468585
myeongju24 <72543116+myeongju24@users.noreply.github.com>
2020-11-19 18:20:24 +0900

    異常系テストコード

--- spec/models/order_form_spec.rb
+++ spec/models/order_form_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe OrderForm do
 
   before do
-    @user = FactoryBot.build(:user)
     @order_form = FactoryBot.build(:order_form)
   end
 
@@ -11,9 +10,65 @@ describe OrderForm do
 
     context '購入がうまくいくとき' do
 
-      it "全ての必須事項を入力すれば購入できる" do
+      it '全ての必須事項を入力すれば購入できる' do
         expect(@order_form).to be_valid
       end
+
+      it '建物名は空でも購入できる' do
+        expect(@order_form).to be_valid
+      end
+    end
+
+    context '購入が上手くいかない時' do
+      
+      it 'tokenが空だと購入できない' do
+        @order_form.token = ""
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Token can't be blank")
+      end
+
+      it '郵便番号が空だと購入できない' do
+        @order_form.postal_code = ""
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Postal code can't be blank")
+      end
+
+      it '郵便番号はハイフンを含んでいないと購入できない'do
+        @order_form.postal_code = "1234567"
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Postal code is invalid. Include hyphen(-)")
+      end
+
+      it '都道府県を選択していないと購入できない' do
+        @order_form.prefecture_id = 1
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Prefecture must be other than 1")
+      end
+
+      it '市区町村が空だと購入できない' do
+        @order_form.city = ""
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("City can't be blank")
+      end
+
+      it '電話番号が空だと購入できない' do
+        @order_form.phone_number = ""
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Phone number can't be blank")
+      end
+
+      it '電話番号が全角数字だと購入できない' do
+        @order_form.phone_number = "０９０１２３４５６７８"
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Phone number Input only number")
+      end
+
+      it '電話番号にハイフンが含まれていると購入できない' do
+        @order_form.phone_number = "090-1234-5678"
+        @order_form.valid?
+        expect(@order_form.errors.full_messages).to include("Phone number Input only number")
+      end
+
     end
     
   end
